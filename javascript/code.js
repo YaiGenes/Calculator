23997d626ac7d853bb64da9a3b3203164e0c485f
Yaiser <yaiserar@gmail.com>
2021-06-28 15:14:05 +0200

    javascript folder

--- javascript/code.js
+++ javascript/code.js
@@ -1,28 +1,31 @@
-const buttons = document.querySelectorAll('button');
-//select buttons and stores in an array
-const result = document.querySelector(".result");
-//select the place to show the result of the operation
+        const buttons = document.querySelectorAll('button');
+        //select buttons and stores in an array
+        const container1 = document.querySelector(".container1");
+        //select the place to show the result of the operation
+        const container2 = document.querySelector(".container2")
 
-buttons.forEach(button => {
-    button.addEventListener("click", operate);
-});
-//add event listener to each button
+        buttons.forEach(button => {
+            button.addEventListener("click", operate);
+        });
+        //add event listener to each button
 
-function operate(event) {
-    //calculator function
-    const clickedButtonValue = event.target.value;
-    //store in the variable the current clicked button´s value
+        function operate(event) {
+            //calculator function
+            const clickedButtonValue = event.target.value;
+            //store in the variable the current clicked button´s value
 
-    if (clickedButtonValue === '=') {
-        //
-        if (result.value !== '') {
-
-            result.value = eval(result.value);
+            if (clickedButtonValue === '=') {
+                //check if the display is not empty
+                if (container1.value !== '') {
+                    container2.value = eval(container1.value);
+                }
+                //calculate and show the answer to display
+            } else if (clickedButtonValue === 'C') {
+                container1.value = '';
+                container2.value = '';
+                //clear everything
+            } else {
+                container1.value += clickedButtonValue;
+                //concatenate it to the display
+            }
         }
-
-    } else if (clickedButtonValue === 'C') {
-        result.value = '';
-    } else {
-        result.value += clickedButtonValue;
-    }
-}
